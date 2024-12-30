833c453b332158f3f395da7fd59a9de03e9c27d9
caigq99 <2961509073@qq.com>
2024-12-30 15:35:14 +0800

    feat: 添加数据库连接

--- server/apps/admin/src/admin.module.ts
+++ server/apps/admin/src/admin.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { DbModule } from '@libs/db';
 
 @Module({
-  imports: [],
+  imports: [DbModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
--- server/libs/db/src/db.module.ts
+++ server/libs/db/src/db.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { DbService } from './db.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin123456',
+      database: 'blogdb',
+      entities: [],
+      synchronize: true, // Don't use this in production
+      autoLoadEntities: true, // auto load entities
+    }),
+  ],
   providers: [DbService],
   exports: [DbService],
 })
